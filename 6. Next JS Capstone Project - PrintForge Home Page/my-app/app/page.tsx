0c61ee7df2a9f8d8931d0df365e94c42bc412b21
Dube Jnr 77 <bubuyadube75@gmail.com>
2026-09-16 11:28:58 +0200

    Capstone project

---
+++ 6. Next JS Capstone Project - PrintForge Home Page/my-app/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Discover what's possible with 3D printing</h1>
+      <p>Join our community of creators and explore a vast library of user-submitted models.</p>
+      <button>Browse Models</button>
+      <img src="/hero-image.png"/>
+    </main>
+  )
+}
